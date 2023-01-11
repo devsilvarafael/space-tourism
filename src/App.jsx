7b1dc424d3bebac671c42d1c9b7981b2587c4206
Rafael Silva <devsilva.rafael@gmail.com>
2023-01-10 23:52:19 -0300

    feat(style): create global style.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+
+
+import "../src/styles/globals.css"
+
 function App() {
     return (
         <div>
-            <h2>Hello World</h2>
+            <h1>Earth</h1>
+            <h2>Crew</h2>
+            <h3>Technology</h3>
+            <h4>Destination</h4>
+            <h5>Solar System</h5>
+            <p>Earth</p>
         </div>
     )
 }
