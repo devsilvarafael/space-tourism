a1396ccaf2389a599137e5792b62695b349f6ec0
Rafael Silva <devsilva.rafael@gmail.com>
2023-01-17 15:48:02 -0300

    feat: create Image component with responsiveness.

---
+++ src/components/commons/Image.jsx
@@ -0,0 +1,22 @@
+export const Image = ({ src, alt, imageConfig }) => {
+    const border = imageConfig.border && 'border-b border-gray-600 md:border-none'
+    let variant;
+
+    if (imageConfig.variant === "medium") {
+        variant = 'md:max-h-[300px] md:max-w-[300px]';
+    }
+
+    if (imageConfig.variant === "large") {
+        variant = 'md:h-[33.25rem]'
+    }
+
+    if (imageConfig.variant === "fullScreenImage") {
+        variant = 'w-screen p-0';
+    }
+
+    return (
+        <figure className={`w-full h-full ${border} ${variant}`}>
+            <img src={`./src/${src}`} alt={alt} className={`max-h-56 md:max-h-full m-auto`}/>
+        </figure>
+    )
+}
