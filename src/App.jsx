60e43df375251cd00d7b9201a65d9cd51e86fec5
Rafael Silva <devsilva.rafael@gmail.com>
2023-01-17 15:53:40 -0300

    chore: Add pages into App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import {Fragment} from "react";
+import Destination from "./pages/Destination.jsx";
+import Home from "./pages/Home.jsx";
+import Crew from "./pages/Crew.jsx";
+import Technology from "./pages/Technology.jsx";
+
 function App() {
     return (
-        <div>
-            <h1 className="bg-light-blue">Earth</h1>
-            <h2>Crew</h2>
-            <h3>Technology</h3>
-            <h4>Destination</h4>
-            <h5>Solar System</h5>
-            <p>Earth</p>
-        </div>
+        <Fragment>
+            <Home />
+            <Crew />
+            <Destination />
+            <Technology />
+        </Fragment>
     )
 }
 
